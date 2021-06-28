6869a1c7542342ee88e6cc7d84cbf6927b2fa6d8
trevorlecroy <trevorlecroy@hotmail.com>
2021-06-27 21:24:08 -0400

    Finished Character.js, and added Styling via styled-components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
+import {setupWorker} from 'msw';
+import styled from 'styled-components';
+
+const CharacterHolder = styled.div`
+  margin: 0px auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -10,22 +21,37 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  
+
   useEffect(() => {
-    axios.get('https://swapi.dev/api/people')
+    setupWorker(axios.get('https://swapi.dev/api/people')
       .then(res => {
-        setCharacters(() => [...res.data])
+        // console.log(res.data);
+        // console.log(res.data.results);
+        
+        setCharacters(() => {
+          if(res.data.results === undefined) {
+            return res.data;
+          }
+          return res.data.results;
+        });
+        
         console.log(characters);
+
       })
       .catch(err => {
         console.log(err);
-      });
-
+      }));
   }, []);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <CharacterHolder>
+        {
+          characters.map(character => {
+            return <Character character={character} key={character.name+character.mass}> </Character>
+        })}
+      </CharacterHolder>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import styled, {css} from "styled-components";
+
+const CharacterCard = styled.div`
+    width: 20em;
+    color: white;
+    background-color: grey;
+    border-radius: 5px;
+    margin-bottom: 10px;
+    padding: 1em;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+`;
+
+const Character = (props) => {
+    console.log(props);
+    return (
+        <CharacterCard>
+            <h2>{props.character.name}</h2>
+            <h3>{props.character.birth_year}</h3>
+        </CharacterCard>
+    );
+}
+
+export default Character;
